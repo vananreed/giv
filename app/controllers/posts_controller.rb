97fb94fe9ac81e78ed49aefd9d5e6f8a0ec116e8
Reed Van Anrooy <reed.vananrooy@gmail.com>
2019-12-07 21:46:08 -0700

    Adds posts#new, posts#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.charity = Charity.first
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :photo)
+  end
 end
